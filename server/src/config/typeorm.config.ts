a11ad9246658f2659e57d64c18fcd0a66d7d858c
Руслан Карымов <ruslan@eljur.team>
2022-08-28 16:01:17 +0300

    server init

---
+++ server/src/config/typeorm.config.ts
@@ -0,0 +1,21 @@
+import { TypeOrmModuleAsyncOptions} from "@nestjs/typeorm";
+import { TypeOrmModuleOptions } from "@nestjs/typeorm/dist/interfaces/typeorm-options.interface";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+
+export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: async (): Promise<TypeOrmModuleOptions> => {
+        return {
+            type: 'postgres',
+            host: process.env.POSTGRES_HOST,
+            port: parseInt(process.env.POSTGRES_PORT, 10),
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DATABASE,
+            entities: ['./dist/**/*.entity.js'],
+            logging: true,
+            synchronize: true,
+        };
+    }
+};
---
+++ server/src/user.entity.ts
@@ -0,0 +1,9 @@
+import { PrimaryGeneratedColumn, Entity, Column } from "typeorm"
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column()
+    name: string;
+}
