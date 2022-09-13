c27b4adf00792332502e74fb03ab3501186a5178
Руслан Карымов <ruslan@eljur.team>
2022-09-13 17:54:41 +0300

    <master> optimize dockerfile

--- up-prod.sh
+++ up-prod.sh
@@ -1,1 +1,1 @@
-docker-compose --env-file ./server/src/config/env/.env.production.local up --build --force-recreate --no-deps
+docker-compose --env-file ./server/src/config/env/.env.production.local up
